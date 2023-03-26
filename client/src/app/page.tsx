6440366cfa192092be0f6fc2ced956077f726b4a
rezaan6 <rezaan6@gmail.com>
2023-03-27 00:39:31 +0530

    update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline";
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20" />
+     <a href="/"><DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20" /></a> 
 
       <h1 className="text-3xl text-center mt-2 text-black font-bold mb-5 ">
         Welcome to the Amazon Scraper
